6dd366d6757dfeffad5926268b06ee87cec3cdd4
Jose <joavila@unal.edu.co>
2023-09-03 21:47:56 -0500

    Added docker support

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
-const express = require('express');
+const express = require("express");
 const app = express();
 const users = require("./routes/users");
 const auth = require("./routes/auth");
 
 // Connect to mongodb
 mongoose
-  .connect("mongodb://127.0.0.1/users")
+  .connect("mongodb://mongodb-container:27017/users", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log("Connected to db");
   })
@@ -18,7 +21,5 @@ app.use(express.json());
 app.use("/api/users", users);
 app.use("/api/auth", auth);
 
-
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
-
