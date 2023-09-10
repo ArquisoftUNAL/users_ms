dfa85ef8c5823858993c5831d3a2079eae3ea1ee
Jose <joavila@unal.edu.co>
2023-09-09 19:48:56 -0500

    Updated route to create users

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,9 @@ const { User, validate } = require("../models/user");
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const _ = require("lodash");
+require("dotenv").config();
 
 // Get user by id
 router.get("/:id", async (req, res) => {
@@ -12,28 +15,37 @@ router.get("/:id", async (req, res) => {
 
 // Create a new user
 router.post("/", async (req, res) => {
+  // Validate user with Joi and mongoose schema
   const { error } = validate(req.body);
   if (error) return res.status(404).send(error.details[0].message);
 
   let user = await User.findOne({ email: req.body.email });
   if (user) return res.status(404).send("User already registered");
 
-  const { name, email, password, age, birthDay, profilePicture } = req.body;
+  const { name, email, password, birthDay, profilePicture } = req.body;
 
   user = new User({
     name,
     email,
     password,
-    age,
     birthDay,
     profilePicture,
   });
 
-  const salt = await bcrypt.genSalt(10);
-  user.password = await bcrypt.hash(user.password, salt);
+  try {
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+  } catch (error) {
+    console.error("Password hashing failed:", error);
+  }
 
   user = await user.save();
-  res.send(user);
+
+  // Generate token and send user to the client
+  const token = jwt.sign({ _id: user._id }, process.env.JWT_PRIVATE_KEY);
+  // Remove password and __v from user object
+  const userWithoutPassword = _.omit(user.toObject(), ["password", "__v"]);
+  res.header("x-auth-token", token).send(userWithoutPassword);
 });
 
 // Delete a user
@@ -45,14 +57,13 @@ router.delete("/:id", async (req, res) => {
 
 // Patch a user
 router.patch("/:id", async (req, res) => {
-  const { name, email, password, age, birthDay, profilePicture } = req.body;
+  const { name, email, password, birthDay, profilePicture } = req.body;
   const user = await User.findByIdAndUpdate(
     req.params.id,
     {
       name,
       email,
       password,
-      age,
       birthDay,
       profilePicture,
     },
